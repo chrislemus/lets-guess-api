4693746afe68f3d4051930c430f558e7fbb37016
chrislemus <icrislemus@gmail.com>
2021-03-24 14:37:36 -0400

    addded game records index

--- app/controllers/game_records_controller.rb
+++ app/controllers/game_records_controller.rb
@@ -1,6 +1,24 @@
 class GameRecordsController < ApplicationController
-
+  def index
+    game_records = GameRecord.all
+    render  json: game_records, except: [:created_at, :updated_at]
+  end 
   def create
+    game_record = GameRecord.new(game_record_params)
+    if game_record.save
+      render json: game_record
+    else
+      render json: {status: "error", code: 4000, message: "missing field"}
+    end
     
   end 
+
+  private
+
+
+  def game_record_params
+    # puts params
+    DeserializablePost.call(json_hash)
+    params.require(:game_record).permit(:username,  :elapsed_time, :phrase_id)
+  end
 end
--- app/models/game_record.rb
+++ app/models/game_record.rb
@@ -1,3 +1,6 @@
 class GameRecord < ApplicationRecord
   belongs_to :phrase
+  validates :username, presence: true
+  validates :elapsed_time, presence: true
+  validates :phrase_id, presence: true
 end
