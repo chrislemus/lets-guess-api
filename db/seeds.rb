5acb0818e78202973041020e104da2b93ba8ead8
chrislemus <icrislemus@gmail.com>
2021-03-31 10:04:08 -0400

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,30 +16,63 @@ technology = Category.create({name: 'technology'})
 
 
 Phrase.create([
-  {
-    phrase: 'duck',
-    category: animals
-  },
-  {
-    phrase: 'football',
-    category: sports
-  },
-  {
-    phrase: 'finding nemo',
-    category: movies
-  },
-  {
-    phrase: 'windshield',
-    category: cars
-  },
-  {
-    phrase: 'computer',
-    category: technology
-  }
+  { phrase: 'duck', category: animals },
+  { phrase: 'cow', category: animals },
+  { phrase: 'chicken', category: animals },
+  { phrase: 'dog', category: animals },
+  { phrase: 'elephant', category: animals },
+  { phrase: 'zebra', category: animals },
+  { phrase: 'leopard', category: animals },
+  { phrase: 'pig', category: animals },
+  { phrase: 'bear', category: animals },
+  { phrase: 'owl', category: animals },
+  { phrase: 'football', category: sports },
+  { phrase: 'soccer', category: sports },
+  { phrase: 'chicago bulls', category: sports },
+  { phrase: 'fifa', category: sports },
+  { phrase: 'super bowl', category: sports },
+  { phrase: 'baseball', category: sports },
+  { phrase: 'tennis', category: sports },
+  { phrase: 'miami heat', category: sports },
+  { phrase: 'lakers', category: sports },
+  { phrase: 'game time', category: sports },
+  { phrase: 'referee', category: sports },
+  { phrase: 'team', category: sports },
+  { phrase: 'finding nemo', category: movies },
+  { phrase: 'die hard', category: movies },
+  { phrase: 'the avengers', category: movies },
+  { phrase: 'spiderman', category: movies },
+  { phrase: 'jurassic park', category: movies },
+  { phrase: 'lion king', category: movies },
+  { phrase: 'ghodtbusters', category: movies },
+  { phrase: 'star wars', category: movies },
+  { phrase: 'matrix', category: movies },
+  { phrase: 'alien', category: movies },
+  { phrase: 'the wizard of oz', category: movies },
+  { phrase: 'windshield', category: cars },
+  { phrase: 'wheel', category: cars },
+  { phrase: 'ferrari', category: cars },
+  { phrase: 'rolls royce', category: cars },
+  { phrase: 'toyota', category: cars },
+  { phrase: 'tesla', category: cars },
+  { phrase: 'gas pedal', category: cars },
+  { phrase: 'windshield wipers', category: cars },
+  { phrase: 'gas', category: cars },
+  { phrase: 'oil', category: cars },
+  { phrase: 'door handle', category: cars },
+  { phrase: 'trunk', category: cars },
+  { phrase: 'computer',  category: technology },
+  { phrase: 'programming',  category: technology },
+  { phrase: 'internet',  category: technology },
+  { phrase: 'wifi',  category: technology },
+  { phrase: 'social network',  category: technology },
+  { phrase: 'game console',  category: technology },
+  { phrase: 'monitor',  category: technology },
+  { phrase: 'keyboard',  category: technology },
+  { phrase: 'sound system',  category: technology },
+  { phrase: 'google',  category: technology },
+  { phrase: 'tv',  category: technology },
+  { phrase: 'solar panel',  category: technology },
+  { phrase: 'phone',  category: technology },
 ])
 
-GameRecord.create({
-  username: 'chris',
-  elapsed_time: 1.2,
-  phrase_id: Phrase.first.id
-})
